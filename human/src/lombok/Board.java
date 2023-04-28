9aecfac7c223e4151440042e553c765205348368
seedyoo <lukdown10@gmail.com>
2023-04-28 16:57:53 +0900

    lombok

---
+++ human/src/lombok/Board.java
@@ -0,0 +1,34 @@
+package lombok;
+
+import java.util.Date;
+
+// @ : 어노테이션
+@Data
+public class Board {
+	
+	private int boardNo;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regDate;
+	private Date updDate;
+	
+
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ human/src/lombok/test.java
@@ -0,0 +1,14 @@
+package lombok;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Board board = new Board();
+		
+		board.setBoardNo(10);
+		
+		
+		
+	}
+}
